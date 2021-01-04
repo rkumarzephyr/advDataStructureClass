8e034a10be9ce0b9bea809a4c301a5e0bf7da4a0
rkumarzephyr <50817463+rkumarzephyr@users.noreply.github.com>
2021-01-04 16:23:52 +0530

    Add files via upload

--- AlgorithmClassCpp/src/MaxHeap.cpp
+++ AlgorithmClassCpp/src/MaxHeap.cpp
@@ -6,6 +6,9 @@
  */
 
 #include "MaxHeap.h"
+#include <iostream>
+
+using namespace std;
 
 MaxHeap::MaxHeap(int arr[], int size) {
 	for(int i= 0; i < size; i++)
@@ -19,8 +22,10 @@ MaxHeap::~MaxHeap() {
 
 void MaxHeap::heapify(void)
 {
-   for(int i= heap_size/2; i <= 0; i--)
+   for(int i= heap_size/2; i >= 0; i--)
+   {
 	   heapify(i);
+   }
 }
 void MaxHeap::heapify(int i)
 {
@@ -34,7 +39,7 @@ void MaxHeap::heapify(int i)
    else
 	   largestIndex = i;
 
-   if(rightIndex < heap_size && array[rightIndex] > array[i])
+   if(rightIndex < heap_size && array[rightIndex] > array[largestIndex])
 	   largestIndex = rightIndex;
 
    if(largestIndex != i)
@@ -47,8 +52,77 @@ void MaxHeap::heapify(int i)
    }
 }
 
+/**
+ * implements merge sort as mentioned
+ * in cormen. this method can be called again and it
+ * will work, ideally we can remember that a list is
+ * already sort, like a boolean isAlreadySorted.
+ */
 void MaxHeap::sort()
 {
    heapify();
+   // max element is at array[0]
+   int temp = 0;
+   int size = heap_size;
 
+   while(heap_size > 0)
+   {
+	   temp = array[heap_size-1];
+	   array[heap_size-1] = array[0];
+	   array[0] = temp;
+	   heap_size--;
+	   heapify(0);
+   }
+
+   heap_size = size;
+}
+
+/**
+ * call this method on heapfied tree
+ * and not on sorted one.
+ */
+int MaxHeap::heap_extract_maximum()
+{
+   if(heap_size < 1)
+	   throw ("error");
+
+   int max = array[0];
+   array[0] = array[heap_size-1];
+   heap_size--;
+   heapify(0);
+   return max;
+}
+
+void MaxHeap::increaseKey(int i,int key)
+{
+	if(!(i < heap_size))
+		throw ("invalid key");
+
+	array[i] = key;
+
+	while ((i > 0 && array[i] > array[parent(i)])) {
+
+		exchange(i,parent(i));
+		i = parent(i);
+	}
+}
+
+/**
+ * insert new leaf node and then
+ * have to make sure heap property is
+ * maintained by going to parent.
+ * in production code we also should check for
+ * capacity of internal array.
+ */
+void MaxHeap::insert(int key)
+{
+	array[heap_size] = 0;
+	heap_size++;
+	increaseKey(heap_size-1, key);
+}
+void MaxHeap::print()
+{
+	cout<<endl;
+	for(int i= 0; i < heap_size;i++)
+		cout<<array[i]<<" ";
 }
--- AlgorithmClassCpp/src/MaxHeap.h
+++ AlgorithmClassCpp/src/MaxHeap.h
@@ -11,7 +11,27 @@
 #define MAX_SIZE 100
 class MaxHeap {
 	int array[MAX_SIZE];
-	int heap_size;
+	int heap_size; // gets updated after the sort.
+	/**
+	 * note these methods are different than
+	 * cormen since index here starts from 0
+	 */
+	int left(int i) {
+		return 2 * i + 1;
+	}
+	int right(int i) {
+		return 2 * i + 2;
+	}
+	int parent(int i) {
+		return (i - 1) / 2;
+	}
+
+	void exchange(int i, int j)
+	{
+       int temp = array[i];
+       array[i] = array[j];
+       array[j] = temp;
+	}
 public:
 	MaxHeap(int arr[],int size);
 	virtual ~MaxHeap();
@@ -20,22 +40,21 @@ public:
 
 	void sort();
 
+	void print();
+
 	/**
-	 * note these methods are different than
-	 * cormen since index here starts from 0
+	 * extracts the maximum element
+	 * and then makes sure that the rest follows
+	 * heap property
 	 */
-	int left(int i)
-	{
-		return 2*i + 1;
-	}
-	int right(int i)
-	{
-		return 2*i+2;
-	}
-	int parent(int i)
-	{
-		return (i+1)/2;
-	}
+	int heap_extract_maximum();
+
+	/**
+	 * increases the value of key pointed by index.
+	 */
+	void increaseKey(int index,int value);
+
+	void insert(int key);
 };
 
 #endif /* MAXHEAP_H_ */
